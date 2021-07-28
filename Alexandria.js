85e035c4358e3a7a221a54fd0684a21032d33dac
mc-pixel <mrcelma3@gmail.com>
2021-07-28 20:55:55 +0200

    Skeleton of the project

---
+++ Alexandria.js
@@ -0,0 +1,1 @@
+const trivia = ["The Library of Ashurbanipal The world's oldest known library was founded sometime in the 7th century B.C. for the “royal contemplation” of the Assyrian ruler Ashurbanipal. Located in Nineveh in modern day Iraq, the site included a trove of some 30,000 cuneiform tablets organized according to subject matter.",]
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project-Alexandria
+
+This is a skeleton of the page currently Im working on the database with firebase
