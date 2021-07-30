741728326b50d7a4e0809a373cc9b1a0c0acf4e4
mc-pixel <mrcelma3@gmail.com>
2021-07-30 03:10:03 +0200

    carousel checkpoint

--- Alexandria.js
+++ Alexandria.js
@@ -1,1 +1,11 @@
-const trivia = ["The Library of Ashurbanipal The world's oldest known library was founded sometime in the 7th century B.C. for the “royal contemplation” of the Assyrian ruler Ashurbanipal. Located in Nineveh in modern day Iraq, the site included a trove of some 30,000 cuneiform tablets organized according to subject matter.",]
+const slider = tns({
+  container: ".js-carousel",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  mouseDrag: true,
+  controlsText: [
+    '<i class="fas fa-chevron-left"></i>',
+    '<i class="fas fa-chevron-right"></i>'
+  ]
+});
