2951d0590a5897463ab83bc10080b7938d02b4a4
mc-pixel <mrcelma3@gmail.com>
2021-08-01 16:46:28 +0200

    checkpoint still some problems linking firebase

--- Alexandria.js
+++ Alexandria.js
@@ -9,3 +9,24 @@ const slider = tns({
     '<i class="fas fa-chevron-right"></i>'
   ]
 });
+firebase.initializeApp({
+  apiKey: 'AIzaSyCqJJvdwD5QLuY0NtSJtlw8AgcJDGkj_eU',
+  authDomain: 'project-alexandria-613cb.firebaseapp.com',
+  projectId: 'project-alexandria-613cb'
+});
+
+var db = firebase.firestore();
+var booksRef = db.collection("Books");
+
+var query = booksRef.where("Author", "==", "Isaac Asimov");
+
+it("should get all books", () => {
+  var output = db.collection("Books").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(`${doc.id} => ${doc.data()}`);
+                });
+            });
+            console.log(output);
+               return output;
+                             });
+
